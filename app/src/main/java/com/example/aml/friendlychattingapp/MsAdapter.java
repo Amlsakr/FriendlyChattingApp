7ec2b01079cdb5121595b04e6cfab77bf422661a
Amlsakr <amlsakr9@gmail.com>
2017-09-25 17:36:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/aml/friendlychattingapp/MsAdapter.java
@@ -0,0 +1,56 @@
+package com.example.aml.friendlychattingapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+/**
+ * Created by aml on 09/09/17.
+ */
+
+
+public class MsAdapter extends ArrayAdapter<MessageModel> {
+
+
+    public MsAdapter(Context context, int resource, List<MessageModel> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.item_message, parent,
+                    false);
+        }
+
+        ImageView photoImageView = (ImageView) convertView.findViewById(R.id.imageView);
+        TextView messageTextView = (TextView) convertView.findViewById(R.id.messageBody);
+        TextView authorTextView = (TextView) convertView.findViewById(R.id.message_creator);
+
+        MessageModel message = getItem(position);
+
+        boolean isPhoto = message.getPhotoUrl() != null;
+        if (isPhoto) {
+            messageTextView.setVisibility(View.GONE);
+            photoImageView.setVisibility(View.VISIBLE);
+            Glide.with(photoImageView.getContext())
+                    .load(message.getPhotoUrl())
+                    .into(photoImageView);
+        } else {
+            messageTextView.setVisibility(View.VISIBLE);
+            photoImageView.setVisibility(View.GONE);
+            messageTextView.setText(message.getText());
+        }
+        authorTextView.setText(message.getName());
+
+        return convertView;
+    }
+}
